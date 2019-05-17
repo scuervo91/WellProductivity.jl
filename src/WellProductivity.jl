f306097471d3a8ad7217a5b4f26fe93faed64898
Santiago Cuervo <scuervo91@gmail.com>
2019-05-17 11:32:45 -0500

    Files generated by PkgTemplates

---
+++ src/WellProductivity.jl
@@ -0,0 +1,5 @@
+module WellProductivity
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using WellProductivity
+using Test
+
+@testset "WellProductivity.jl" begin
+    # Write your own tests here.
+end
